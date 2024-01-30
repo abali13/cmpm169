eeb790a3005dc2df9ad7ad3f4630aeea341d4a01
abali13 <abali1@ucsc.edu>
2024-01-29 23:22:13 -0800

    Experiment 3 code

--- experiment3/js/sketch.js
+++ experiment3/js/sketch.js
@@ -1,12 +1,13 @@
-// sketch.js - purpose and description here
-// Author: Your Name
-// Date:
+// sketch.js - Experiment 3
+// Author: Aditya Bali
+// Date: 1/29/24
 
 // Here is how you might set up an OOP p5.js project
 // Note that p5.js looks for a file called sketch.js
 
 // Constants - User-servicable parts
 // In a longer project I like to put these in a separate file
+/*
 const VALUE1 = 1;
 const VALUE2 = 2;
 
@@ -65,3 +66,118 @@ function draw() {
 function mousePressed() {
     // code to run when mouse is pressed
 }
+*/
+// M_1_5_03
+//
+// Generative Gestaltung – Creative Coding im Web
+// ISBN: 978-3-87439-902-9, First Edition, Hermann Schmidt, Mainz, 2018
+// Benedikt Groß, Hartmut Bohnacker, Julia Laub, Claudius Lazzeroni
+// with contributions by Joey Lee and Niels Poldervaart
+// Copyright 2018
+//
+// http://www.generative-gestaltung.de
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+/**
+ * noise values (noise 3d) are used to animate a bunch of agents.
+ *
+ * KEYS
+ * 1-2                 : switch noise mode
+ * space               : new noise seed
+ * backspace           : clear screen
+ * s                   : save png
+ */
+'use strict';
+var c;
+function setup(){
+   c = color(150, 180, 195);
+}
+
+var sketch = function(p) {
+
+  var agents = [];
+  var agentCount = 3000;
+  var noiseScale = 200;
+  var noiseStrength = 10;
+  var noiseZRange = 0.4;
+  var noiseZVelocity = 0.01;
+  var overlayAlpha = 200;
+  var agentAlpha = 90;
+  var strokeWidth = 0.3;
+  var drawMode = 1;
+
+  p.setup = function() {
+    p.createCanvas(p.windowWidth, p.windowHeight);
+
+    for (var i = 0; i < agentCount; i++) {
+      agents[i] = new Agent(noiseZRange);
+    }
+  };
+
+  p.draw = function() {
+    p.fill(255, overlayAlpha);
+    p.noStroke();
+    p.rect(0, 0, p.width, p.height);
+    
+    
+     if (p.key == 'c'){
+       c = color(random(255), random(255), random(255), random(255));
+      //drawMode = 1;
+      //p.stroke(c, agentAlpha);
+       //c = color(noiseScale, noiseStrength, noiseZVelocity);
+       //c = noise(noiseScale, );
+    } 
+     // line
+    p.stroke(0,130,164);
+    p.noFill();
+
+    var noiseXRange = p.mouseX / 10;
+    //console.log('noiseXRange: 0 - ' + noiseXRange);
+
+    p.beginShape();
+    p.stroke(c, agentAlpha);
+    //strokeWeight(100);
+    for (var x = 0; x < p.width; x += 10) {
+      var noiseX = p.map(x, 0, p.width, 0, noiseXRange);
+      var y = p.noise(noiseX) * p.height;
+      p.vertex(x,y); 
+      //p.vertex(strokeWidth, noiseScale);
+    };
+    p.endShape();
+
+    // Draw agents
+    p.stroke(c, agentAlpha);
+    for (var i = 0; i < agentCount; i++) {
+      if (drawMode == 1) {
+        //agents[i].update1(noiseXRange, p.mouseY, noiseStrength, noiseZVelocity);
+        agents[i].update1(strokeWidth, noiseScale, noiseStrength, noiseZVelocity);
+      } else {
+        agents[i].update2(strokeWidth, noiseScale, noiseStrength, noiseZVelocity);
+      }
+    }
+  };
+
+  p.keyReleased = function() {
+    if (p.key == 's' || p.key == 'S') p.saveCanvas(gd.timestamp(), 'png');
+    if (p.key == '1') drawMode = 1;
+    if (p.key == '2') drawMode = 2;
+    if (p.key == '3') drawMode = 30;
+    if (p.key == ' ') {
+      var newNoiseSeed = p.floor(p.random(10000));
+      console.log('newNoiseSeed', newNoiseSeed);
+      p.noiseSeed(newNoiseSeed);
+    }
+    if (p.keyCode == p.DELETE || p.keyCode == p.BACKSPACE) p.background(255);
+  };
+ 12
+};
+
+var myp5 = new p5(sketch);
