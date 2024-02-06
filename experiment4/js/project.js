4e06b780a8282c94eed3005308487dc1bfa49f6a
abali13 <abali1@ucsc.edu>
2024-02-06 12:39:16 -0800

    Changes

--- experiment4/js/project.js
+++ experiment4/js/project.js
@@ -7,28 +7,49 @@
 // Constants - User-servicable parts
 // In a longer project I like to put these in a separate file
 
-// define a class
-class MyProjectClass {
-  // constructor function
-  constructor(param1, param2) {
-    // set properties using 'this' keyword
-    this.property1 = param1;
-    this.property2 = param2;
+let video;
+var overlayAlpha = 200;
+var imgCount = 0;
+var count = 10;
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  background(100);
+
+  // Create a video capture object
+  video = createCapture(VIDEO);
+  video.size(80, 60); // Set the size of the video frame
+  video.hide(); // Hide the captured video
+}
+
+function draw() {
+  // Display the video frame as a stamp at the mouse position
+  //var x = floor(random(100));
+  //console.log(x);
+  
+  /*
+  if((x % 2) == 0){
+    console.log("Goes in");
+     background(100);
+  }else{
+    //console.log("Doesnt go in");
+  }
+  */
+  if(imgCount >= count){
+    background(100);
+    imgCount = 0;
   }
   
-  // define a method
-  myMethod() {
-    // code to run when method is called
+  if(mouseIsPressed){
+    count++;
+    console.log(count);
+  }else{
+    count =  10;
   }
+ 
+  
+  image(video, mouseX - 40, mouseY - 30, mouseX, mouseY);
+  imgCount++;
 }
 
-function main() {
-  // create an instance of the class
-  let myInstance = new MyProjectClass(value1, value2);
 
-  // call a method on the instance
-  myInstance.myMethod();
-}
-
-// let's get this party started - uncomment me
-//main();
